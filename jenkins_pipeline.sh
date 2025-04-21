8aefc38ce07c63dcd326236f5ec30c57e31567cd
mishka2134 <mzefa2014@gmail.com>
2025-04-22 00:47:10 +0500

    Update jenkins_pipeline.sh

--- jenkins_pipeline.sh
+++ jenkins_pipeline.sh
@@ -1,3 +1,4 @@
+#№1 Download - скачивание датасета
 sudo apt-get update # Обновим список пакетов
 sudo apt-get install -y python3.12-venv # Установим venv для Python 3.12
 python3 -m venv ./my_env # Создадим виртуальное окружение
@@ -7,16 +8,19 @@ pip3 install setuptools # Установим setuptools
 pip3 install -r requirements.txt # Установим зависимости
 python3 download.py # Запустим скрипт загрузки
 
+#№2 train_model - тренировка модели
 . /var/lib/jenkins/workspace/download/my_env/bin/activate
 cd /var/lib/jenkins/workspace/download # Перейдем в рабочую директорию
 python3 train_model.py > best_model.txt # Обучение модели и сохранение вывода
 
+#№3 deploy
 . /var/lib/jenkins/workspace/download/my_env/bin/activate
 cd /var/lib/jenkins/workspace/download 
 export BUILD_ID=dontKillMe # Запретим Jenkins убивать процесс
 path_model=$(cat best_model.txt | grep -oP '(?<=Путь к лучшей модели: ).*') # Извлечем путь к модели
 mlflow models serve -m "$path_model" -p 5003 --no-conda & # Запустим сервис MLflow на порту 5003
 
+#№4 healthy
 . /var/lib/jenkins/workspace/download/my_env/bin/activate 
 cd /var/lib/jenkins/workspace/download 
 curl http://127.0.0.1:5003/invocations -H"Content-Type:application/json" --data '{"inputs": [[-1.275938045, -1.2340347, -1.41327673, 0.76150439, 2.20097247, -0.410937195, 0.58931542, 0.1135538, 0.58931542]]}' 
